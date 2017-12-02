1ca9d785f3c8743e5aaf9e1eb9b51802ea013da4
amsul <reach@amsul.ca>
2017-12-01 22:02:10 -0500

    Set up the build scripts

---
+++ rollup.config.js
@@ -0,0 +1,36 @@
+import babel from 'rollup-plugin-babel'
+import flow from 'rollup-plugin-flow'
+import nodeResolve from 'rollup-plugin-node-resolve'
+
+export default {
+  input: 'lib/index.js',
+  output: [
+    {
+      file: 'dist/react-translated.js',
+      format: 'es',
+      exports: 'named',
+    },
+    {
+      file: 'dist/react-translated.umd.js',
+      format: 'umd',
+      exports: 'named',
+      name: 'react-translated',
+      globals: {
+        'prop-types': 'PropTypes',
+        'react': 'React',
+      },
+    },
+  ],
+  plugins: [
+    flow(),
+    babel({
+      exclude: 'node_modules/**',
+      presets: ['es2015-rollup', 'stage-0', "react"],
+    }),
+    nodeResolve(),
+  ],
+  external: [
+    'prop-types',
+    'react',
+  ],
+}
